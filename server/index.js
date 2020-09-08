fa0c0e0b0199f86b91856860432b0eeb50018b52
kolodiva <kolodiva@mail.ru>
2020-09-08 11:16:02 +0300

    ok

--- server/index.js
+++ server/index.js
@@ -20,8 +20,8 @@ async function start() {
   } else {
     //const { host, port } = nuxt.options.server;
     //host = '192.168.1.41'; port = 3000;
-    //host = '192.168.1.105'; port = 3000;
-    host = 'localhost'; port = 6000;
+    host = '192.168.1.105'; port = 3000;
+    //host = 'localhost'; port = 6000;
   }
 
   await nuxt.ready()
