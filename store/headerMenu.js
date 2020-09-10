f5620ab0c284edf67e7371c9bfaf4076b4c7a07b
kolodiva <kolodiva@mail.ru>
2020-09-10 22:05:33 +0300

    ok

--- store/headerMenu.js
+++ store/headerMenu.js
@@ -1,11 +1,13 @@
 import _ from "lodash";
 import header1 from "@/assets/data/header1.json";
 import header2 from "@/assets/data/header2.json";
+import header3 from "@/assets/data/header3.json";
 import addresses from "@/assets/data/addresses.json";
 
 export const state = () => ({
   header1: header1.data,
   header2: header2.data,
+  header3: header3.data,
   addresses: addresses.address,
 });
 
@@ -15,6 +17,9 @@ export const getters = {
   },
   getHeader2: (state) => {
     return state.header2;
+  },
+  getHeader3: (state) => {
+    return state.header3;
   },
   getAllSortCity: (state) => {
     const list = _.sortBy(state.addresses, "id");
