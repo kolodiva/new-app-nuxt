c2d3be7a189cf3afdaa90ae5aeeeb2431aaf6d1a
kolodiva <kolodiva@mail.ru>
2020-10-10 12:05:04 +0300

    ok

--- components/Nomenklator/TheGoodsList.vue
+++ components/Nomenklator/TheGoodsList.vue
@@ -2,145 +2,45 @@
   <v-container fluid>
     <v-row>
       <v-col md="2" class="hidden-sm-and-down">
-        <div id="sidebar1">
-          <div>
-            <v-expansion-panels v-model="openPanel1" focusable multiple>
-              <v-expansion-panel v-for="(item, i) in 1" :key="i">
-                <v-expansion-panel-header>
-                  Разделы каталога
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-            <v-expansion-panels
-              v-model="openPanel2"
-              focusable
-              multiple
-              class="mt-4"
-            >
-              <v-expansion-panel v-for="(item, i) in 1" :key="i">
-                <v-expansion-panel-header>
-                  Отбор по параметрам
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-          </div>
-        </div>
+        <TheGoodsListLeftSideBar />
       </v-col>
 
       <v-col md="10" sm="12" class="pt-0">
-        <v-toolbar dense flat rounded bordered>
-          <v-toolbar-title class="body-2">Сортировать по:</v-toolbar-title>
-
-          <v-toolbar-title class="body-2 font-weight-medium ml-4"
-            >по алфавиту</v-toolbar-title
-          >
-          <v-divider
-            vertical
-            class="mx-2 my-auto"
-            inset
-            style="height: 16px"
-          ></v-divider>
-          <v-toolbar-title class="body-2">популярности</v-toolbar-title>
-          <v-divider
-            vertical
-            class="mx-2 my-auto"
-            inset
-            style="height: 16px"
-          ></v-divider>
-          <v-toolbar-title class="body-2">цене</v-toolbar-title>
-          <v-divider
-            vertical
-            class="mx-2 my-auto"
-            inset
-            style="height: 16px"
-          ></v-divider>
-          <v-toolbar-title class="body-2">новизне</v-toolbar-title>
-          <v-divider
-            vertical
-            class="mx-2 my-auto"
-            inset
-            style="height: 16px"
-          ></v-divider>
-          <v-toolbar-title class="body-2">скидке</v-toolbar-title>
-
-          <v-spacer></v-spacer>
-
-          <v-toolbar-title class="body-2">Показать по:</v-toolbar-title>
-
-          <v-toolbar-title class="body-2 font-weight-medium ml-4"
-            >30</v-toolbar-title
-          >
-          <v-divider
-            vertical
-            class="mx-2 my-auto"
-            inset
-            style="height: 16px"
-          ></v-divider>
-          <v-toolbar-title class="body-2">60</v-toolbar-title>
-          <v-divider
-            vertical
-            class="mx-2 my-auto"
-            inset
-            style="height: 16px"
-          ></v-divider>
-          <v-toolbar-title class="body-2">90</v-toolbar-title>
-          <v-divider
-            vertical
-            class="mx-2 my-auto"
-            inset
-            style="height: 16px"
-          ></v-divider>
-
-          <v-spacer></v-spacer>
-
-          <v-toolbar-title class="body-2">Вид каталога:</v-toolbar-title>
-
-          <v-toolbar-title class="body-2 ml-4"
-            ><v-icon color="black">mdi-apps </v-icon></v-toolbar-title
-          >
-          <v-toolbar-title class="body-2 ml-4"
-            ><v-icon>mdi-format-list-checkbox </v-icon></v-toolbar-title
-          >
-          <v-toolbar-title class="body-2 ml-4"
-            ><v-icon>mdi-view-headline</v-icon></v-toolbar-title
-          >
-        </v-toolbar>
-
+        <TheGoodsListMenu />
         <v-sheet rounded="lg" class="mt-4" min-height="100vh">
-          <v-row align="start" justify="center">
-            <n-link
-              v-for="(pos, id) in subNomenklator"
-              :key="id"
-              :to="`${pos.guid}`"
-              style="text-decoration: none"
-            >
-              <v-card class="ma-1" color="grey lighten-4" max-width="300">
+          <v-row>
+            <v-col v-for="(pos, id) in subNomenklator" :key="id" class="pa-3">
+              <v-card
+                class=""
+                color="grey lighten-4"
+                max-width="300"
+                height="100%"
+                style="display: flex; flex-direction: column"
+              >
                 <v-hover v-slot:default="{ hover }">
-                  <v-img :src="`${pos.guid_picture}`" contain>
+                  <v-img
+                    :src="`${pos.guid_picture}`"
+                    contain
+                    style="flex-grow: 1"
+                  >
                     <v-expand-transition>
                       <div
                         v-if="hover"
-                        class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                        class="d-flex flex-column transition-fast-in-fast-out orange darken-2 v-card--reveal subtitle-1 white--text"
                         style="height: 100%"
                       >
-                        {{ pos.price1 }}
+                        <p>розн. {{ pos.price1 }}</p>
+                        <p>мелк опт. {{ pos.price2 }}</p>
+                        <p>круп опт. {{ pos.price3 }}</p>
                       </div>
                     </v-expand-transition>
                   </v-img>
                 </v-hover>
-                <v-card-text class="pt-6" style="position: relative">
+
+                <v-card-text
+                  class="pt-6"
+                  style="position: relative; flex-grow: 4"
+                >
                   <v-btn
                     absolute
                     color="orange"
@@ -155,12 +55,12 @@
                   <div class="font-weight-light grey--text title mb-2">
                     {{ pos.artikul }}
                   </div>
-                  <h3 class="display-1 font-weight-light orange--text mb-2">
+                  <h3 class="heading-4 font-weight-light orange--text mb-2">
                     {{ pos.name }}
                   </h3>
                 </v-card-text>
 
-                <v-card-actions>
+                <v-card-actions style="flex-grow: 1">
                   <v-btn color="orange lighten-2" text> Описание </v-btn>
 
                   <v-spacer></v-spacer>
@@ -180,7 +80,7 @@
                   </div>
                 </v-expand-transition>
               </v-card>
-            </n-link>
+            </v-col>
           </v-row>
         </v-sheet>
       </v-col>
@@ -204,7 +104,7 @@ export default {
     }),
   },
   mounted() {
-    window.$("#sidebar1").stickr({ duration: 0, offsetTop: 80 });
+    // window.$("#sidebar1").stickr({ duration: 0, offsetTop: 80 });
     // $('#sidebar2').stickr({duration:0, offsetTop: 80});
   },
 };
