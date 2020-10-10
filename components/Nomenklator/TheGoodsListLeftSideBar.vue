2a9ad6d391a520122a38701477790b122a2fd97e
kolodiva <kolodiva@mail.ru>
2020-10-10 12:23:00 +0300

    ok

--- components/Nomenklator/TheGoodsListLeftSideBar.vue
+++ components/Nomenklator/TheGoodsListLeftSideBar.vue
@@ -7,6 +7,7 @@
             Разделы каталога
           </v-expansion-panel-header>
           <v-expansion-panel-content>
+            
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
