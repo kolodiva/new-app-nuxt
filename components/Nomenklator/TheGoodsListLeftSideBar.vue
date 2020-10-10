44c2a7c935a12d4d414e3836dba6eef43a573bab
kolodiva <kolodiva@mail.ru>
2020-10-10 12:11:48 +0300

    ok

---
+++ components/Nomenklator/TheGoodsListLeftSideBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="sidebar1">
+    <div>
+      <v-expansion-panels v-model="openPanel1" focusable multiple>
+        <v-expansion-panel v-for="(item, i) in 1" :key="i">
+          <v-expansion-panel-header>
+            Разделы каталога
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      <v-expansion-panels v-model="openPanel2" focusable multiple class="mt-4">
+        <v-expansion-panel v-for="(item, i) in 1" :key="i">
+          <v-expansion-panel-header>
+            Отбор по параметрам
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      openPanel1: [0],
+      openPanel2: [0],
+      show: false,
+    };
+  },
+  computed: {
+    // ...mapGetters({
+    //   subNomenklator: "nomenklator/getSubNomenklator",
+    // }),
+  },
+  mounted() {
+    window.$("#sidebar1").stickr({ duration: 0, offsetTop: 80 });
+  },
+};
+</script>
+
+<style></style>
---
+++ components/Nomenklator/TheGoodsListMenu.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-toolbar dense flat rounded bordered>
+    <v-toolbar-title class="body-2">Сортировать по:</v-toolbar-title>
+
+    <v-toolbar-title class="body-2 font-weight-medium ml-4"
+      >по алфавиту</v-toolbar-title
+    >
+    <v-divider
+      vertical
+      class="mx-2 my-auto"
+      inset
+      style="height: 16px"
+    ></v-divider>
+    <v-toolbar-title class="body-2">популярности</v-toolbar-title>
+    <v-divider
+      vertical
+      class="mx-2 my-auto"
+      inset
+      style="height: 16px"
+    ></v-divider>
+    <v-toolbar-title class="body-2">цене</v-toolbar-title>
+    <v-divider
+      vertical
+      class="mx-2 my-auto"
+      inset
+      style="height: 16px"
+    ></v-divider>
+    <v-toolbar-title class="body-2">новизне</v-toolbar-title>
+    <v-divider
+      vertical
+      class="mx-2 my-auto"
+      inset
+      style="height: 16px"
+    ></v-divider>
+    <v-toolbar-title class="body-2">скидке</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-title class="body-2">Показать по:</v-toolbar-title>
+
+    <v-toolbar-title class="body-2 font-weight-medium ml-4">30</v-toolbar-title>
+    <v-divider
+      vertical
+      class="mx-2 my-auto"
+      inset
+      style="height: 16px"
+    ></v-divider>
+    <v-toolbar-title class="body-2">60</v-toolbar-title>
+    <v-divider
+      vertical
+      class="mx-2 my-auto"
+      inset
+      style="height: 16px"
+    ></v-divider>
+    <v-toolbar-title class="body-2">90</v-toolbar-title>
+    <v-divider
+      vertical
+      class="mx-2 my-auto"
+      inset
+      style="height: 16px"
+    ></v-divider>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-title class="body-2">Вид каталога:</v-toolbar-title>
+
+    <v-toolbar-title class="body-2 ml-4"
+      ><v-icon color="black">mdi-apps </v-icon></v-toolbar-title
+    >
+    <v-toolbar-title class="body-2 ml-4"
+      ><v-icon>mdi-format-list-checkbox </v-icon></v-toolbar-title
+    >
+    <v-toolbar-title class="body-2 ml-4"
+      ><v-icon>mdi-view-headline</v-icon></v-toolbar-title
+    >
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+};
+</script>
+
+<style></style>
