d7b3559c9a4af6e1a3c03dba54e5ed411305f3bb
kolodiva <kolodiva@mail.ru>
2020-10-10 12:15:08 +0300

    ok

--- components/Nomenklator/TheSubNomenklator.vue
+++ components/Nomenklator/TheSubNomenklator.vue
@@ -2,41 +2,7 @@
   <v-container fluid>
     <v-row>
       <v-col md="2" class="hidden-sm-and-down">
-        <div id="sidebar1">
-          <div>
-            <v-expansion-panels v-model="openPanel1" focusable multiple>
-              <v-expansion-panel v-for="(item, i) in 1" :key="i">
-                <v-expansion-panel-header>
-                  Разделы каталога
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-            <v-expansion-panels
-              v-model="openPanel2"
-              focusable
-              multiple
-              class="mt-4"
-            >
-              <v-expansion-panel v-for="(item, i) in 1" :key="i">
-                <v-expansion-panel-header>
-                  Отбор по параметрам
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-          </div>
-        </div>
+        <TheGoodsListLeftSideBar />
       </v-col>
 
       <v-col md="8" sm="12" class="pt-0">
