baa8b5f6a092baaaac7dc740e2d81de508a587b1
kolodiva <kolodiva@mail.ru>
2020-10-12 13:27:19 +0300

    ok

--- components/Nomenklator/TheGoodsListQuickShop.vue
+++ components/Nomenklator/TheGoodsListQuickShop.vue
@@ -44,7 +44,7 @@ export default {
     }),
   },
   mounted() {
-    console.log(this.paramsQuickShopDialog);
+    // console.log(this.paramsQuickShopDialog);
   },
   methods: {
     async closeQuickShopDialog() {
