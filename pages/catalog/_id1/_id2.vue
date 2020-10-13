4621cc45d9acc8213a114cac533bd04119b425a9
kolodiva <kolodiva@mail.ru>
2020-10-13 17:04:13 +0300

    ok

--- pages/catalog/_id1/_id2.vue
+++ pages/catalog/_id1/_id2.vue
@@ -1,144 +1,97 @@
 <template>
-  <v-app>
-    <div>
-      <v-row style="height: 200px" align="center"
-        ><v-col
-          ><h2
-            style="text-align: center"
-            @click="$vuetify.goTo('#dashboard_100')"
-          >
-            {{ pos.name }}
-          </h2></v-col
-        ></v-row
-      >
-      <v-row style=""
-        ><v-col class="leftColumn" cols="3" style="border: 1px solid black">
-          <div
-            id="sidebar1"
-            style="
-              transform: translate(0, 0);
-              transform: translate3d(0, 0, 0);
-              will-change: position, transform;
-            "
-          >
-            <h2 style="text-align: center">Левый блок</h2>
-            <p style="cursor: pointer" @click="$vuetify.goTo('#dashboard_149')">
-              Перейти в конец
-            </p>
-            <p style="cursor: pointer" @click="$vuetify.goTo('#dashboard_75')">
-              Перейти в середину
-            </p>
-            <p style="cursor: pointer" @click="$vuetify.goTo('#dashboard_0')">
-              Перейти в начало
-            </p>
-            <h2 style="text-align: center">Этапы прохождения</h2>
-            <p
-              style="cursor: pointer"
-              :style="{ fontWeight: pos50 ? 'bold' : '400' }"
-            >
-              Пройдено 50
-            </p>
-            <p
-              style="cursor: pointer"
-              :style="{ fontWeight: pos100 ? 'bold' : '400' }"
-            >
-              Пройдено 100
-            </p>
-            <p
-              style="cursor: pointer"
-              :style="{ fontWeight: pos150 ? 'bold' : '400' }"
-            >
-              Пройдено 150
-            </p>
-          </div>
-        </v-col>
-        <v-col cols="6" style="border: 1px solid black" class="menuHeader3"
-          ><v-card
-            id="dashboard_0"
-            v-intersect="{
-              handler: onIntersect,
-              options: {
-                threshold: [0, 0.5, 1.0],
-              },
-            }"
-          >
-            <v-img
-              :src="`${pos.guid_picture.replace('_250x250', '')}`"
-              contain
-              width="600"
-            >
-            </v-img>
+  <v-container fluid>
+    <h2 class="ma-2" style="">
+      {{ pos.name }}
+    </h2>
 
-            <v-card-text>Центр_1</v-card-text></v-card
-          >
-
-          <v-card
-            id="dashboard_75"
-            v-intersect="{
-              handler: onIntersect,
-              options: {
-                threshold: [0, 0.5, 1.0],
-              },
-            }"
-            style="height: 50vh"
-            ><v-card-text>Центр_50</v-card-text></v-card
-          >
-
-          <v-card
-            id="dashboard_149"
-            v-intersect="{
-              handler: onIntersect,
-              options: {
-                threshold: [0, 0.5, 1.0],
-              },
-            }"
-            style="height: 50vh"
-            ><v-card-text>Центр_100</v-card-text></v-card
-          >
-
-          <v-card
-            id="dashboard_100"
-            v-intersect="{
-              handler: onIntersect,
-              options: {
-                threshold: [0, 0.5, 1.0],
-              },
-            }"
-            style="height: 50vh"
-            ><v-card-text>Центр_149</v-card-text></v-card
-          >
-        </v-col>
-        <v-col
-          id="sidebar2"
-          cols="3"
-          style="border: 1px solid black; height: 50vh"
-          class="rightColumn"
+    <v-row clas="">
+      <v-col md="2" class="grey lighten-4 hidden-sm-and-down pr-0 pt-0">
+        <div
+          id="sidebar1"
+          style="
+            transform: translate(0, 0);
+            transform: translate3d(0, 0, 0);
+            will-change: position, transform;
+          "
         >
-          <div
-            id="sidebar2"
-            style="
-              transform: translate(0, 0);
-              transform: translate3d(0, 0, 0);
-              will-change: position, transform;
-            "
-          >
-            <h2 style="text-align: center">Правый блок</h2>
-            <p style="cursor: pointer" @click="$vuetify.goTo('#dashboard_149')">
-              Перейти в конец
-            </p>
-            <p style="cursor: pointer" @click="$vuetify.goTo('#dashboard_75')">
-              Перейти в середину
-            </p>
-            <p style="cursor: pointer" @click="$vuetify.goTo('#dashboard_0')">
-              Перейти в начало
-            </p>
+          <div>
+            <v-tabs
+              v-model="cur_tab"
+              class="ml-auto"
+              background-color="grey lighten-4"
+              vertical
+              style="margin-top: 2px; width: 70%"
+              active-class="background-color: white"
+            >
+              <v-tabs-slider color="transparent"></v-tabs-slider>
+              <v-tab style="height: 30px" @click="$router.back()">
+                <v-spacer /> К списку
+              </v-tab>
+              <v-tab style="height: 30px" @click="goTo('#section_1')">
+                <v-spacer /> Обзор
+              </v-tab>
+              <v-tab style="height: 30px" @click="goTo('#section_2')">
+                <v-spacer /> Описание
+              </v-tab>
+              <v-tab style="height: 30px" @click="goTo('#section_3')">
+                <v-spacer /> Характеристики
+              </v-tab>
+              <v-tab style="height: 30px" @click="goTo('#section_4')">
+                <v-spacer /> Инструкции и файлы
+              </v-tab>
+            </v-tabs>
           </div>
-        </v-col>
-      </v-row>
+        </div>
+      </v-col>
 
-      <v-footer style="height: 100vh">Footer</v-footer>
-    </div>
-  </v-app>
+      <v-col
+        id="section_0"
+        v-intersect="{
+          handler: onIntersect,
+          options: {
+            threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+          },
+        }"
+        md="8"
+        sm="12"
+        class="pt-0 pl-0 grey lighten-4"
+      >
+        <v-card id="section_1" flat style="margin-top: 2px">
+          <v-img
+            :src="`${pos.guid_picture.replace('_250x250', '')}`"
+            contain
+            max-width="300"
+          />
+        </v-card>
+
+        <v-card id="section_2" style="height: 15vh" class="mt-1" flat
+          ><v-card-text>Описание</v-card-text></v-card
+        >
+
+        <v-card id="section_3" style="height: 50vh" class="mt-1" flat
+          ><v-card-text>Характеристики</v-card-text></v-card
+        >
+        <v-card id="section_4" style="height: 15vh" class="mt-1" flat
+          ><v-card-text>Инструкции</v-card-text></v-card
+        >
+      </v-col>
+      <v-col cols="2" style="" class="grey lighten-4">
+        <div
+          id="sidebar2"
+          style="
+            transform: translate(0, 0);
+            transform: translate3d(0, 0, 0);
+            will-change: position, transform;
+          "
+        >
+          <div>
+            <p>Артикул: {{ pos.artikul }}, {{ pos.artikul_new }}</p>
+            <p class="">Цена: {{ pos.price1 }}</p>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -154,6 +107,7 @@ export default {
     pos50: false,
     pos100: false,
     pos150: false,
+    cur_tab: 1,
   }),
   computed: {
     ...mapGetters({
@@ -161,23 +115,48 @@ export default {
     }),
   },
   mounted() {
-    window.$("#sidebar1").stickr({ duration: 0, offsetTop: 50 });
-    window.$("#sidebar2").stickr({ offsetTop: 50 });
+    window.$("#sidebar1").stickr({ duration: 0, offsetTop: 55 });
+    window.$("#sidebar2").stickr({ duration: 0, offsetTop: 55 });
   },
   methods: {
+    goTo(element) {
+      // this.$hello(window.$(element).position().top);
+      this.$vuetify.goTo(window.$(element).position().top - 50);
+    },
+
     onIntersect(entries, observer) {
       // More information about these options
       // is located here: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
       // this.isIntersecting = entries[0].intersectionRatio >= 0.5
-      if (entries[0].target.textContent === "Центр_50") {
-        this.pos50 = entries[0].intersectionRatio >= 0.5;
-      }
-      if (entries[0].target.textContent === "Центр_100") {
-        this.pos100 = entries[0].intersectionRatio >= 1;
-      }
-      if (entries[0].target.textContent === "Центр_149") {
-        this.pos150 = entries[0].intersectionRatio >= 1;
-      }
+      const wScrL = window.$(window).scrollTop();
+      const addVal = 5;
+
+      const curBlock = [
+        window.$("#section_1").offset().top - wScrL + addVal,
+        window.$("#section_2").offset().top - wScrL + addVal,
+        window.$("#section_3").offset().top - wScrL + addVal,
+        window.$("#section_4").offset().top - wScrL + addVal,
+      ];
+      // this.$hello("--- " + curBlock.id.split("_")[1]);
+
+      // this.$hello(curBlock.indexOf(curBlock.find((val) => val > 0)));
+
+      this.cur_tab = curBlock.indexOf(curBlock.find((val) => val > 0)) + 1;
+
+      // if (curBlock.sec >= 1) {
+      //   const res = curBlock.id.split("_")[1];
+      //   this.cur_tab = parseInt(res);
+      //   // this.$hello(this.cur_tab);
+      // }
+      // if (entries[0].target.textContent === "Центр_50") {
+      //   this.pos50 = entries[0].intersectionRatio >= 0.5;
+      // }
+      // if (entries[0].target.textContent === "Центр_100") {
+      //   this.pos100 = entries[0].intersectionRatio >= 1;
+      // }
+      // if (entries[0].target.textContent === "Центр_149") {
+      //   this.pos150 = entries[0].intersectionRatio >= 1;
+      // }
       // consola.info(entries[0].target.innerText, entries[0].intersectionRatio)
     },
   },
