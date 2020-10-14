91b5204236acc95c2c03323fe310d510c1d5c86f
kolodiva <kolodiva@mail.ru>
2020-10-14 15:57:43 +0300

    ok

---
+++ components/Nomenklator/TheCardGoodBigView.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-dialog
+    v-model="openDialog"
+    max-width="80vw"
+    @click:outside="closeDialog"
+    @keydown="closeDialog"
+  >
+    <v-card>
+      <v-card-title class="headline"> Тэсто амадесто </v-card-title>
+
+      <v-img :src="`${photos.pic_path.replace('_250x250', '')}`" width="600" />
+
+      <v-card-text>
+        Здесь вы можете Хорошо подумать и принять самое ответственное решение в
+        своей жизни - купить ЭТО или оставить на потом когда Цены подрастут?
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="green darken-1" text @click="closeDialog">
+          спасибо, не надо...
+        </v-btn>
+        <v-spacer></v-spacer>
+
+        <v-btn color="green darken-1" text @click="closeDialog">
+          в корзину
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ["openDialog", "photos"],
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {
+    // console.log(this.paramsQuickShopDialog);
+  },
+  methods: {
+    closeDialog() {
+      this.$emit("closeWin");
+    },
+  },
+};
+</script>
+
+<style></style>
--- pages/catalog/_id1/_id2.vue
+++ pages/catalog/_id1/_id2.vue
@@ -1,129 +1,137 @@
 <template>
-  <v-container v-scroll="onIntersect" fluid>
-    <v-row class="">
-      <v-col class="grey lighten-4 hidden-sm-and-down pr-0 pt-0">
-        <div
-          id="sidebar1"
-          style="
-            transform: translate(0, 0);
-            transform: translate3d(0, 0, 0);
-            will-change: position, transform;
-          "
-        >
-          <div>
-            <v-tabs
-              v-model="cur_tab"
-              class="ml-auto"
-              background-color="grey lighten-4"
-              vertical
-              style="margin-top: 2px"
-              active-class="background-color: white"
-            >
-              <v-tabs-slider color="transparent"></v-tabs-slider>
-              <v-tab style="height: 30px" @click="$router.back()">
-                <v-spacer /> К списку
-              </v-tab>
-              <v-tab style="height: 30px" @click="goTo('#section_1')">
-                <v-spacer /> Обзор
-              </v-tab>
-              <v-tab style="height: 30px" @click="goTo('#section_2')">
-                <v-spacer /> Описание
-              </v-tab>
-              <v-tab style="height: 30px" @click="goTo('#section_3')">
-                <v-spacer /> Характеристики
-              </v-tab>
-              <v-tab style="height: 30px" @click="goTo('#section_4')">
-                <v-spacer /> Инструкции и файлы
-              </v-tab>
-            </v-tabs>
-          </div>
-        </div>
-      </v-col>
-
-      <v-col md="8" sm="12" class="pt-0 pl-0 grey lighten-4">
-        <v-card id="section_1" flat style="margin-top: 2px">
-          <v-card-text class="">{{ pos.name }}</v-card-text>
-          <v-row align="center">
-            <v-col class="pt-0">
-              <v-tabs height="0" :value="cur_tab_photos">
-                <v-tab-item v-for="(photo, i) in photos" :key="i">
-                  <v-hover v-slot:default="{ hover }">
-                    <v-img
-                      :src="`${photo.pic_path.replace('_250x250', '')}`"
-                      contain
-                      height="400"
-                      class="mx-auto"
-                    >
-                      <v-row class="fill-height flex-column" justify="center">
-                        <div class="align-self-center">
-                          <v-btn
-                            :class="{ 'show-btns': hover }"
-                            color="transparent"
-                            icon
-                          >
-                            <v-icon x-large :class="{ greyMfText: hover }">
-                              mdi-magnify-plus
-                            </v-icon></v-btn
-                          >
-                        </div>
-                      </v-row>
-                    </v-img>
-                  </v-hover>
-                </v-tab-item>
-              </v-tabs>
-              <v-tabs centered @change="(id) => (cur_tab_photos = id)">
-                <v-tab v-for="(photo, i) in photos" :key="i">
-                  <v-img :src="`${photo.pic_path}`" contain max-width="55" />
+  <div>
+    <v-container v-scroll="onIntersect" fluid>
+      <v-row class="">
+        <v-col class="grey lighten-4 hidden-sm-and-down pr-0 pt-0">
+          <div
+            id="sidebar1"
+            style="
+              transform: translate(0, 0);
+              transform: translate3d(0, 0, 0);
+              will-change: position, transform;
+            "
+          >
+            <div>
+              <v-tabs
+                v-model="cur_tab"
+                class="ml-auto"
+                background-color="grey lighten-4"
+                vertical
+                style="margin-top: 2px"
+                active-class="background-color: white"
+              >
+                <v-tabs-slider color="transparent"></v-tabs-slider>
+                <v-tab style="height: 30px" @click="$router.back()">
+                  <v-spacer /> К списку
+                </v-tab>
+                <v-tab style="height: 30px" @click="goTo('#section_1')">
+                  <v-spacer /> Обзор
+                </v-tab>
+                <v-tab style="height: 30px" @click="goTo('#section_2')">
+                  <v-spacer /> Описание
+                </v-tab>
+                <v-tab style="height: 30px" @click="goTo('#section_3')">
+                  <v-spacer /> Характеристики
+                </v-tab>
+                <v-tab style="height: 30px" @click="goTo('#section_4')">
+                  <v-spacer /> Инструкции и файлы
                 </v-tab>
               </v-tabs>
-            </v-col>
-
-            <v-col>
-              <v-simple-table dense>
-                <template v-slot:default>
-                  <tbody>
-                    <tr v-for="item in characts" :key="item.field">
-                      <td style="border-bottom: 1px dotted #cccccc">
-                        {{ item.field }}
-                      </td>
-                      <td style="border-bottom: 0px">{{ pos[item.val] }}</td>
-                    </tr>
-                  </tbody>
-                </template>
-              </v-simple-table>
-            </v-col>
-          </v-row>
-        </v-card>
-
-        <v-card id="section_2" style="min-height: 20vh" class="mt-1" flat>
-          <v-card-title>Описание</v-card-title>
-          <v-card-text v-html="pos.describe"></v-card-text
-        ></v-card>
-
-        <v-card id="section_3" style="height: 50vh" class="mt-1" flat
-          ><v-card-text>Характеристики</v-card-text></v-card
-        >
-        <v-card id="section_4" style="height: 15vh" class="mt-1" flat
-          ><v-card-text>Инструкции</v-card-text></v-card
-        >
-      </v-col>
-      <v-col style="" class="grey lighten-4">
-        <div
-          id="sidebar2"
-          style="
-            transform: translate(0, 0);
-            transform: translate3d(0, 0, 0);
-            will-change: position, transform;
-          "
-        >
-          <div>
-            <p>Артикул: {{ pos.artikul }}, {{ pos.artikul_new }}</p>
-            <p class="">Цена: {{ pos.price1 }}</p>
+            </div>
           </div>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-col>
+
+        <v-col md="8" sm="12" class="pt-0 pl-0 grey lighten-4">
+          <v-card id="section_1" flat style="margin-top: 2px">
+            <v-card-text class="">{{ pos.name }}</v-card-text>
+            <v-row align="center">
+              <v-col class="pt-0">
+                <v-tabs height="0" :value="cur_tab_photos">
+                  <v-tab-item v-for="(photo, i) in photos" :key="i">
+                    <v-hover v-slot:default="{ hover }">
+                      <v-img
+                        :src="`${photo.pic_path.replace('_250x250', '')}`"
+                        contain
+                        height="400"
+                        class="mx-auto"
+                      >
+                        <v-row class="fill-height flex-column" justify="center">
+                          <div class="align-self-center">
+                            <v-btn
+                              :class="{ 'show-btns': hover }"
+                              color="transparent"
+                              icon
+                              @click="openDialogBigView = true"
+                            >
+                              <v-icon x-large :class="{ greyMfText: hover }">
+                                mdi-magnify-plus
+                              </v-icon></v-btn
+                            >
+                          </div>
+                        </v-row>
+                      </v-img>
+                    </v-hover>
+                  </v-tab-item>
+                </v-tabs>
+                <v-tabs centered @change="(id) => (cur_tab_photos = id)">
+                  <v-tab v-for="(photo, i) in photos" :key="i">
+                    <v-img :src="`${photo.pic_path}`" contain max-width="55" />
+                  </v-tab>
+                </v-tabs>
+              </v-col>
+
+              <v-col>
+                <v-simple-table dense>
+                  <template v-slot:default>
+                    <tbody>
+                      <tr v-for="item in characts" :key="item.field">
+                        <td style="border-bottom: 1px dotted #cccccc">
+                          {{ item.field }}
+                        </td>
+                        <td style="border-bottom: 0px">{{ pos[item.val] }}</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </v-col>
+            </v-row>
+          </v-card>
+
+          <v-card id="section_2" style="min-height: 20vh" class="mt-1" flat>
+            <v-card-title>Описание</v-card-title>
+            <v-card-text v-html="pos.describe"></v-card-text
+          ></v-card>
+
+          <v-card id="section_3" style="height: 50vh" class="mt-1" flat
+            ><v-card-text>Характеристики</v-card-text></v-card
+          >
+          <v-card id="section_4" style="height: 15vh" class="mt-1" flat
+            ><v-card-text>Инструкции</v-card-text></v-card
+          >
+        </v-col>
+        <v-col style="" class="grey lighten-4">
+          <div
+            id="sidebar2"
+            style="
+              transform: translate(0, 0);
+              transform: translate3d(0, 0, 0);
+              will-change: position, transform;
+            "
+          >
+            <div>
+              <p>Артикул: {{ pos.artikul }}, {{ pos.artikul_new }}</p>
+              <p class="">Цена: {{ pos.price1 }}</p>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <TheCardGoodBigView
+      :photos="photos[cur_tab_photos]"
+      :open-dialog="openDialogBigView"
+      @closeWin="closeBigView"
+    />
+  </div>
 </template>
 
 <script>
@@ -145,6 +153,7 @@ export default {
       { field: "Вес, кг", val: "weight" },
       { field: "Цвет", val: "color" },
     ],
+    openDialogBigView: false,
   }),
   computed: {
     ...mapGetters({
@@ -160,6 +169,10 @@ export default {
     goTo(element) {
       // this.$hello(window.$(element).position().top);
       this.$vuetify.goTo(window.$(element).position().top - 50);
+    },
+
+    closeBigView(event) {
+      this.openDialogBigView = false;
     },
 
     onIntersect(entries, observer) {
