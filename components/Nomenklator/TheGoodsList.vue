5ef91febc3b42fbc467c14aed951a2c11f10b7fc
kolodiva <kolodiva@mail.ru>
2020-10-14 13:19:46 +0300

    ok

--- components/Nomenklator/TheGoodsList.vue
+++ components/Nomenklator/TheGoodsList.vue
@@ -40,13 +40,4 @@ export default {
 };
 </script>
 
-<style>
-.v-card--reveal {
-  align-items: center;
-  bottom: 0;
-  justify-content: center;
-  opacity: 0.5;
-  position: absolute;
-  width: 100%;
-}
-</style>
+<style></style>
