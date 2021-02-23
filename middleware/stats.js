9799169a36cf6cfcfbf68ade0d4fc4e917af91d8
kolodiva <kolodiva@mail.ru>
2021-02-23 22:19:56 +0300

    ok

--- middleware/stats.js
+++ middleware/stats.js
@@ -1,109 +1,30 @@
 // const consola = require('consola')
 export default function ({ route, store, redirect }) {
-  // return axios.post('http://my-stats-api.com', {
-  //   url: route.fullPath,
-  // })
-  // consola.info(route.path === undefined || route.path === '/')
-  // store.commit(
-  //   "SET_BACKSPACE_BTN",
-  //   !(route.path === undefined || route.path === "/")
-  // );
-  // console.log(route);
-  // console.log("*******************************");
-  // console.log(route);
+  if (process.server) {
+    console.log(route);
+    let fullPath = "";
+    let newPath = route.path.toLowerCase();
+    let changed = route.path !== newPath;
 
-  if (
-    route &&
-    route.fullPath &&
-    route.fullPath.includes("&") &&
-    !route.fullPath.includes("/?") &&
-    !route.fullPath.includes("?itemcard=")
-  ) {
-    const firstOf = route.fullPath.indexOf("&");
-    let newPath =
-      "/" +
-      route.fullPath.substr(1, firstOf - 1) +
-      "/?" +
-      route.fullPath.substr(firstOf + 1);
-    // consola.info(newPath)
-    if (newPath.slice(-1) === "/") {
-      newPath = newPath.slice(0, -1);
+    if (route.path.endsWith("/")) {
+      changed = true;
+      newPath = route.path.slice(0, -1);
     }
 
-    return redirect(newPath.toLowerCase());
-  }
-
-  if (route && route.fullPath && route.fullPath.includes("?itemcard=")) {
-    let newPath = route.fullPath;
-
-    if (newPath.slice(-1) === "/") {
-      newPath = newPath.slice(0, -1);
-    }
-    newPath = newPath.replace("?itemcard=", "/");
-
-    return redirect(newPath.toLowerCase());
-  }
-
-  if (route && route.fullPath && route.fullPath.includes("///?utm_source=")) {
-    let newPath = route.fullPath;
-
-    if (newPath.slice(-1) === "/") {
-      newPath = newPath.slice(0, -1);
+    if (route.query && route.query.itemcard) {
+      fullPath = newPath + "/" + route.query.itemcard.toLowerCase();
+      if (fullPath.endsWith("/")) {
+        fullPath = fullPath.slice(0, -1);
+      }
+      changed = true;
     }
 
-    newPath = newPath.replace("///?utm_source=", "/?utm_source=");
-
-    return redirect(newPath.toLowerCase());
-  }
-
-  if (route && route.fullPath && route.fullPath.includes("//?utm_source=")) {
-    let newPath = route.fullPath;
-
-    if (newPath.slice(-1) === "/") {
-      newPath = newPath.slice(0, -1);
+    if (changed) {
+      if (fullPath) {
+        redirect(301, fullPath);
+      } else {
+        redirect(301, route.fullPath.replace(route.path, newPath));
+      }
     }
-    newPath = newPath.replace("//?utm_source=", "/?utm_source=");
-
-    return redirect(newPath.toLowerCase());
-  }
-
-  if (
-    route &&
-    route.fullPath &&
-    route.fullPath !== route.fullPath.toLowerCase()
-  ) {
-    const newPath = route.fullPath.toLowerCase();
-    // if (newPath.slice(-1) === "/") {
-    //   newPath = newPath.slice(0, -1);
-    // }
-    return redirect(newPath);
-  }
-
-  if (route && route.fullPath && route.fullPath.slice(-1) === "/") {
-    const newPath = route.fullPath.slice(0, -1);
-    return redirect(newPath.toLowerCase());
-  }
-  if (route && route.path && route.path.includes("/advs/")) {
-    let tmpPath0 = route.path;
-    // const tmpPath1 = route.fullPath.replace(tmpPath0, '').trim()
-
-    const aPath0 = tmpPath0.split("/");
-
-    const aPath1 = aPath0.filter(function (el) {
-      return el !== "";
-    });
-
-    tmpPath0 = "/" + aPath1.join("/");
-
-    if (!tmpPath0.includes(".html")) {
-      tmpPath0 = tmpPath0 + ".html";
-    }
-
-    // consola.info(tmpPath0, tmpPath1)
-    // if (tmpPath1) {
-    //   tmpPath0 = tmpPath0 + '/' + tmpPath1
-    // }
-
-    return redirect(tmpPath0);
   }
 }
--- serverMiddleware/check-malformed.js
+++ serverMiddleware/check-malformed.js
@@ -1,14 +1,14 @@
-export default async (req, res, next) => {
+export default (req, res, next) => {
 
   try {
      decodeURIComponent(req.originalUrl);
-
-     next();
    } catch (e) {
-
      const url = encodeURI(unescape(req.originalUrl));
-
      req.url = url;
+   }
 
-     next();
-   }};
+
+   next();
+
+
+ };
