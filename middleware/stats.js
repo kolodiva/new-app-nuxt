2853e1bc09a5c4c20264241fbdefebcc4ba53ea0
kolodiva <kolodiva@mail.ru>
2021-02-23 22:38:39 +0300

    ok

--- middleware/stats.js
+++ middleware/stats.js
@@ -1,53 +1,56 @@
 // const consola = require('consola')
 export default function ({ route, store, redirect }) {
-  if (process.server) {
-    // console.log(route);
-    let fullPath = "";
-    let advsPath = "";
-    let newPath = route.path.toLowerCase();
-    let changed = route.path !== newPath;
+  // if (process.server) {
+  //console.log(route);
+  if (route.fullPath === "/") {
+    return;
+  }
+  let fullPath = "";
+  let advsPath = "";
+  let newPath = route.path.toLowerCase();
+  let changed = route.path !== newPath;
 
-    if (route.path.endsWith("/")) {
-      changed = true;
-      newPath = route.path.slice(0, -1);
+  if (route.path.endsWith("/")) {
+    changed = true;
+    newPath = route.path.slice(0, -1);
+  }
+
+  if (route.query && route.query.itemcard) {
+    fullPath = newPath + "/" + route.query.itemcard.toLowerCase();
+    if (fullPath.endsWith("/")) {
+      fullPath = fullPath.slice(0, -1);
+    }
+    changed = true;
+  }
+
+  if (route && route.path && route.path.includes("/advs/")) {
+    let tmpPath0 = route.path;
+
+    const aPath0 = tmpPath0.split("/");
+
+    const aPath1 = aPath0.filter(function (el) {
+      return el !== "";
+    });
+
+    tmpPath0 = "/" + aPath1.join("/");
+
+    if (!tmpPath0.includes(".html")) {
+      tmpPath0 = tmpPath0 + ".html";
     }
 
-    if (route.query && route.query.itemcard) {
-      fullPath = newPath + "/" + route.query.itemcard.toLowerCase();
-      if (fullPath.endsWith("/")) {
-        fullPath = fullPath.slice(0, -1);
-      }
-      changed = true;
-    }
+    advsPath = tmpPath0;
 
-    if (route && route.path && route.path.includes("/advs/")) {
-      let tmpPath0 = route.path;
+    changed = true;
+  }
 
-      const aPath0 = tmpPath0.split("/");
-
-      const aPath1 = aPath0.filter(function (el) {
-        return el !== "";
-      });
-
-      tmpPath0 = "/" + aPath1.join("/");
-
-      if (!tmpPath0.includes(".html")) {
-        tmpPath0 = tmpPath0 + ".html";
-      }
-
-      advsPath = tmpPath0;
-
-      changed = true;
-    }
-
-    if (changed) {
-      if (advsPath) {
-        redirect(301, advsPath);
-      } else if (fullPath) {
-        redirect(301, fullPath);
-      } else {
-        redirect(301, route.fullPath.replace(route.path, newPath));
-      }
+  if (changed) {
+    if (advsPath) {
+      redirect(301, advsPath);
+    } else if (fullPath) {
+      redirect(301, fullPath);
+    } else {
+      redirect(301, route.fullPath.replace(route.path, newPath));
     }
   }
+  // }
 }
