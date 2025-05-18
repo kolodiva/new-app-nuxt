e1efed3f0e2307c903d120be0d93d43210b5908a
kolodiva <kolodiva@mail.ru>
2025-05-18 22:44:16 +0300

    Add 3D models section and related assets to the service page

---
+++ pages/service/3dmodels.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container class="" style="height: 80vh">
+    <v-card flat>
+      <v-card-title> 3D Модели </v-card-title>
+      <v-divider />
+      <v-row>
+        <v-card flat class="pa-3">
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/napravl_sharik_MF_1.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style="max-width: 135px"
+            ><span>3D Модели<br />МФ-Комплект</span></v-card-subtitle
+          >
+        </v-card>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+// if (typeof require !== "undefined") XLSX = require("xlsx");
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    // const workbook = window.XLSX.readFile("testExcel.xlsx");
+    // console.log(workbook);
+  },
+
+  methods: {
+    downloadResource(path) {
+      window.open(path);
+    },
+  },
+  head: {},
+};
+</script>
+
+<style scoped></style>
