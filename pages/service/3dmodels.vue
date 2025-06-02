384ffdf4b927ad760a50bd6ad4a923de0f074fba
kolodiva <kolodiva@mail.ru>
2025-06-02 12:21:44 +0300

    ok

--- pages/service/3dmodels.vue
+++ pages/service/3dmodels.vue
@@ -54,6 +54,56 @@
           >
         </v-card>
 
+        <v-card flat class="pa-3">
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/napravl_skryt_typa_clipon_polnogo_vidvigenia_s_dovodchikom_s_sinhr_3dregulirovkoi_CS.0003D.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style="max-width: 140px"
+            ><span>CS.000.3D Направляющие скрытого типа Clip-on полного выдв. с доводчиком с синх. 3D регулировкой</span></v-card-subtitle
+          >
+        </v-card>
+
+        <v-card flat class="pa-3">
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/MF_napravl_sharikovie_skrytogo_typaDMI_803SHD.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style="max-width: 140px"
+            ><span>MF Направляющие шариковые скрытого типа DMI-803SHD</span></v-card-subtitle
+          >
+        </v-card>
+
 
       </v-row>
     </v-card>
