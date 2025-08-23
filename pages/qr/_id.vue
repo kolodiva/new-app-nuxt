5a63ac8e9107b1a21fa105343fc6ecb83ea9313b
kolodiva <kolodiva@mail.ru>
2025-08-23 03:00:12 +0300

    aaa

--- pages/qr/_id.vue
+++ pages/qr/_id.vue
@@ -5,41 +5,41 @@
         {{ posparams[0].artikul }}, {{ posparams[0].artikul_new }} <br/> {{ posparams[0].name }}
       </v-toolbar-title>
     </v-toolbar>
-    <v-tabs vertical grow style="min-height: 300px;">
+    <v-tabs vertical grow style="min-height: 250px;">
       <v-tab>
         <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '_250x250.jpg'}`"
           :alt="`${posparams[0].name}`"
-          style="max-width: 75px; object-fit: contain;">
+          style="max-width: 60px; object-fit: contain;">
       </v-tab>
       <v-tab>
         <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '_250x250.jpg'}`"
           :alt="`${posparams[0].name}`"
-          style="max-width: 75px; object-fit: contain;">
+          style="max-width: 60px; object-fit: contain;">
       </v-tab>
       <v-tab>
         <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '_250x250.jpg'}`"
           :alt="`${posparams[0].name}`"
-          style="max-width: 75px; object-fit: contain;">
+          style="max-width: 60px; object-fit: contain;">
       </v-tab>
 
       <v-tab-item>
         <v-card flat v-if="posparams && posparams.length > 0">
           <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
             :alt="`${posparams[0].name}`"
-            style="min-height: 300px; max-width: 300px; cursor: pointer;"> </v-card>
+            style="min-height: 250px; max-width: 250px; cursor: pointer;"> </v-card>
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
           <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
             :alt="`${posparams[0].name}`"
-            style="min-height: 300px; max-width: 300px; cursor: pointer;">
+            style="min-height: 250px; max-width: 250px; cursor: pointer;">
         </v-card>
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
           <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
             :alt="`${posparams[0].name}`"
-            style="min-height: 300px; max-width: 300px; cursor: pointer;">
+            style="min-height: 250px; max-width: 250px; cursor: pointer;">
         </v-card>
       </v-tab-item>
     </v-tabs>
