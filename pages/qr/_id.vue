68c5a3c65e0418ae36e4be49199a127a6cf0f8ab
kolodiva <kolodiva@mail.ru>
2025-08-23 03:11:03 +0300

    ee

--- pages/qr/_id.vue
+++ pages/qr/_id.vue
@@ -2,81 +2,80 @@
   <v-card class="mx-auto mt-6" max-width="600px" v-if="posparams && posparams.length > 0">
     <v-toolbar flat color="primary" dark>
       <v-toolbar-title style="font-size: 0.875rem; font-weight: 400; height: 55px; letter-spacing: 0.00714286em;">
-        {{ posparams[0].artikul }}, {{ posparams[0].artikul_new }} <br/> {{ posparams[0].name }}
+        {{ posparams[0].artikul }}, {{ posparams[0].artikul_new }} <br /> {{ posparams[0].name }}
       </v-toolbar-title>
     </v-toolbar>
     <v-tabs vertical grow style="min-height: 250px;">
       <v-tab>
         <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '_250x250.jpg'}`"
-          :alt="`${posparams[0].name}`"
-          style="max-width: 60px; object-fit: contain;">
+          :alt="`${posparams[0].name}`" style="max-width: 60px; object-fit: contain;">
       </v-tab>
       <v-tab>
         <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '_250x250.jpg'}`"
-          :alt="`${posparams[0].name}`"
-          style="max-width: 60px; object-fit: contain;">
+          :alt="`${posparams[0].name}`" style="max-width: 60px; object-fit: contain;">
       </v-tab>
       <v-tab>
         <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '_250x250.jpg'}`"
-          :alt="`${posparams[0].name}`"
-          style="max-width: 60px; object-fit: contain;">
+          :alt="`${posparams[0].name}`" style="max-width: 60px; object-fit: contain;">
       </v-tab>
 
-      <v-tab-item>
-        <v-card flat v-if="posparams && posparams.length > 0">
-          <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
-            :alt="`${posparams[0].name}`"
-            style="min-height: 250px; max-width: 250px; cursor: pointer;"> </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card flat>
-          <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
-            :alt="`${posparams[0].name}`"
-            style="min-height: 250px; max-width: 250px; cursor: pointer;">
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card flat>
-          <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
-            :alt="`${posparams[0].name}`"
-            style="min-height: 250px; max-width: 250px; cursor: pointer;">
-        </v-card>
-      </v-tab-item>
+      <v-row>
+        <v-col>
+          <v-tab-item>
+            <v-card flat v-if="posparams && posparams.length > 0" class="d-block mx-auto">
+              <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
+                :alt="`${posparams[0].name}`" style="min-height: 250px; max-width: 250px; cursor: pointer;"> </v-card>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card flat>
+              <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
+                :alt="`${posparams[0].name}`" style="min-height: 250px; max-width: 250px; cursor: pointer;">
+            </v-card>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card flat>
+              <img :src="`${'https://www.newfurnitura.ru/upload/' + posparams[0].guid + '.jpg'}`"
+                :alt="`${posparams[0].name}`" style="min-height: 250px; max-width: 250px; cursor: pointer;">
+            </v-card>
+          </v-tab-item>
+        </v-col>
+
+        <v-col v-if="posparams && posparams.length > 0" class="ma-3">
+          <v-simple-table dense class="">
+            <template v-slot:default>
+              <tbody>
+                <tr v-for="item in characts" :key="item.field">
+                  <td style="border-bottom: 1px dotted #cccccc">
+                    {{ item.field }}
+                  </td>
+                  <td style="border-bottom: 0px">
+                    {{ posparams[0][item.val] }}
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+          <v-simple-table dense class="">
+            <template v-slot:default>
+              <tbody>
+                <tr v-for="item in characts1" :key="item.field">
+                  <td style="border-bottom: 1px dotted #cccccc">
+                    {{ item.field }}
+                  </td>
+                  <td style="border-bottom: 0px">
+                    {{ posparams[0][item.val] }}
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-col>
+      </v-row>
     </v-tabs>
 
-                    <v-row v-if="posparams && posparams.length > 0" class="my-3 mx-3">
-                      <v-col>
-                      <v-simple-table dense class="">
-                        <template v-slot:default>
-                          <tbody>
-                            <tr v-for="item in characts" :key="item.field">
-                              <td style="border-bottom: 1px dotted #cccccc">
-                                {{ item.field }}
-                              </td>
-                              <td style="border-bottom: 0px">
-                                {{ posparams[0][item.val] }}
-                              </td>
-                            </tr>
-                          </tbody>
-                        </template>
-                      </v-simple-table>
-                      </v-col>
-                      <v-simple-table dense class="">
-                        <template v-slot:default>
-                          <tbody>
-                            <tr v-for="item in characts1" :key="item.field">
-                              <td style="border-bottom: 1px dotted #cccccc">
-                                {{ item.field }}
-                              </td>
-                              <td style="border-bottom: 0px">
-                                {{ posparams[0][item.val] }}
-                              </td>
-                            </tr>
-                          </tbody>
-                        </template>
-                      </v-simple-table>
-                      </v-col>
-                    </v-row>
+    <v-row v-if="posparams && posparams.length > 0" class="my-3 mx-3">
+
+    </v-row>
 
 
 
@@ -91,17 +90,17 @@ export default {
     return {
       posparams: [],
       characts: [
-      { field: "Наименование упак.", val: "packing_name" },
-      { field: "Кол-во в упак.", val: "qty_pack" },
-      { field: "Кол-во на палете.", val: "qty_palette" },
-      { field: "Вес (нетто) кг.", val: "weight_netto_pack" },
-      { field: "Вес (брутто) кг.", val: "weight_brutto_pack" },
-    ],
+        { field: "Наименование упак.", val: "packing_name" },
+        { field: "Кол-во в упак.", val: "qty_pack" },
+        { field: "Кол-во на палете.", val: "qty_palette" },
+        { field: "Вес (нетто) кг.", val: "weight_netto_pack" },
+        { field: "Вес (брутто) кг.", val: "weight_brutto_pack" },
+      ],
       characts1: [
-      { field: "Ширина упак.см" , val: "width_pack" },
-      { field: "Глубина упак.см", val: "depth_pack" },
-      { field: "Высота упак.см", val: "height_pack" },
-    ],
+        { field: "Ширина упак.см", val: "width_pack" },
+        { field: "Глубина упак.см", val: "depth_pack" },
+        { field: "Высота упак.см", val: "height_pack" },
+      ],
     }
   },
 
