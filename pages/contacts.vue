a0067414c037671668ec8eabdfbf6807ddbb462c
kolodiva <kolodiva@mail.ru>
2025-12-11 09:46:03 +0300

    ss

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -80,20 +80,6 @@
                                 />
                               </a>
 
-                              <a
-                                v-if="item2.whatsapp && item.name == 'МФ-КОМПЛЕКТ РОСТОВ-НА-ДОНУ'"
-                                :href="'https://max.ru/?ysclid=mizo8w0gu842156465'"
-                                target="_blank"
-                                title="Напишите нам в онлайн чат"
-                              >
-                                <img
-                                  class="socialIcon fab"
-                                  src="/icons8-max-48.jpg"
-                                  style="width: 28px; height: 28px"
-                                />
-                              </a>
-
-
                             </div>
                           </td>
                           <td>
